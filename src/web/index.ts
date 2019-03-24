b7121a77fe0e70ad52746eed6869d0c3ddce59a5
Henrique Marques Fernandes <henrique@marquesfernandes.com>
2019-03-23 21:52:44 -0300

    Make it better

--- src/web/index.ts
+++ src/web/index.ts
@@ -1,9 +1,17 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+
 import { Server } from 'http';
+import { decode } from 'punycode';
 
 interface CustomExpress extends express.Express {
     /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
     [prop: string]: any;
+}
+
+interface RequestPrivate extends express.Request {
+    /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+    token: { [x: string]: any };
 }
 
 interface Header {
@@ -14,15 +22,21 @@ interface Header {
 interface Options {
     port?: number;
     headers: Header[];
+    secret?: string;
 }
 
 class Web {
     app: CustomExpress;
     port: number = 8000;
     headers: Header[] = [];
+    secret: string = '123456789';
+    enabled: boolean = false;
 
     constructor(options: Options) {
         this.app = express();
+        this.secret = options.secret;
+        this.port = options.port;
+        this.headers = options.headers;
     }
 
     /**
@@ -48,6 +62,13 @@ class Web {
         next();
     }
 
+    private _403 = (res: express.Response) => res.status(403).json({
+        error: {
+            code: 403,
+            message: 'Not authorized',
+        },
+    });
+
     /**
      *
      * @param req
@@ -59,8 +80,19 @@ class Web {
             return (req: express.Request, res: express.Response, next: express.NextFunction): void => next();
         }
         // Route is secure, check the bearer token
-        return (req: express.Request, res: express.Response, next: express.NextFunction): void => {
-            next();
+        return (req: RequestPrivate, res: express.Response, next: express.NextFunction): void => {
+            const { authorization } = req.headers;
+            if (!authorization || authorization.indexOf('Bearer ') === -1) {
+                this._403(res);
+                return;
+            }
+
+            const token = authorization.replace('Bearer ', '');
+
+            jwt.verify(token, this.secret, (error, decoded) => {
+                req.token = decoded as object;
+                next();
+            });
         }
     }
 
