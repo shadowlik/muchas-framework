bbc13edfb3cec810114111d554dbd9d6ad59956e
Henrique Marques Fernandes <henrique@marquesfernandes.com>
2019-03-24 16:06:04 -0300

    Started Log Class

---
+++ src/log/index.ts
@@ -0,0 +1,35 @@
+import Winston from 'winston';
+import uniqid from 'uniqid';
+
+class Log {
+    logger = Winston;
+
+    constructor() {
+
+    }
+
+    /**
+     *
+     */
+    private errorId(): string {
+        return uniqid();
+    };
+
+    /**
+     *
+     * @param message
+     * @param errorCode
+     * @param meta
+     */
+    error(message: string, errorCode?: string, meta?: {}): string {
+        const uid = this.errorId();
+        this.logger.error(message, {
+            uid,
+            errorCode,
+            ...meta,
+        })
+        return uid;
+    }
+};
+
+export = Log;
