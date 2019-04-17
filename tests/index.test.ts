756dbca89fad3e148915dada15fa23790348b472
Henrique Marques Fernandes <henrique@marquesfernandes.com>
2019-04-17 16:38:34 -0300

    #

---
+++ tests/index.test.ts
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const chaiAsPromised = require("chai-as-promised");
+import chai from 'chai';
+
+// import { LocationIq } from '../src/';
+
+chai.use(chaiAsPromised);
+
+const expect: any = chai.expect;
