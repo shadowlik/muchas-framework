c14a0fce7747cbaabd1ccb0e58c8df222b7ec3fa
Henrique Marques Fernandes <henrique@marquesfernandes.com>
2019-05-26 18:55:35 -0300

    Improved server error class

--- src/web/ServerError.ts
+++ src/web/ServerError.ts
@@ -1,8 +1,15 @@
+import uid from 'uniqid';
+
 export class ServerError extends Error {
     uid: string;
-    code: number;
+    code: number | string;
+    status: number;
 
-    constructor(m: string) {
+    constructor(m: string, code: number | string, status: number) {
         super(m);
+
+        this.code = code;
+        this.status = status;
+        this.uid = uid();
     }
 }
