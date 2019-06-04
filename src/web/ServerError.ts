c37cc68287e20d73b1715da77c8aace2bca2ee89
Henrique Fernandes <hfernandes@loyall.local>
2019-06-04 10:01:55 -0300

    #

--- src/web/ServerError.ts
+++ src/web/ServerError.ts
@@ -4,12 +4,14 @@ export class ServerError extends Error {
     uid: string;
     code: number | string;
     status: number;
+    meta: any;
 
-    constructor(m: string, code: number | string, status: number, uid?: string) {
+    constructor(m: string, code: number | string, status: number, uid?: string, meta?: any) {
         super(m);
 
         this.code = code;
         this.status = status;
         this.uid = uid || uniqid();
+        this.meta = meta;
     }
 }
