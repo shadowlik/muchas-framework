7bb2acd19b06c8d04d1d39aee5cbf65bcfee777f
Henrique Fernandes <hfernandes@loyall.local>
2019-06-04 09:59:26 -0300

    #

--- src/web/ServerError.ts
+++ src/web/ServerError.ts
@@ -1,15 +1,15 @@
-import uid from 'uniqid';
+import uniqid from 'uniqid';
 
 export class ServerError extends Error {
     uid: string;
     code: number | string;
     status: number;
 
-    constructor(m: string, code: number | string, status: number) {
+    constructor(m: string, code: number | string, status: number, uid?: string) {
         super(m);
 
         this.code = code;
         this.status = status;
-        this.uid = uid();
+        this.uid = uid || uniqid();
     }
 }
