7370084b5bc3130f583c96d449cacee37fd44f65
Henrique Fernandes <hfernandes@loyall.local>
2019-08-28 16:03:38 -0300

    #

--- src/Redis.ts
+++ src/Redis.ts
@@ -2,8 +2,6 @@ import { promisify } from 'util';
 
 import redis, { RedisClient } from 'redis';
 
-const client = redis.createClient(6379);
-
 class Redis {
     redisInstance: RedisClient;
     get: any;
@@ -11,8 +9,8 @@ class Redis {
 
     constructor(uri: string) {
         this.redisInstance = redis.createClient(uri);
-        this.get = promisify(client.get).bind(this.redisInstance);
-        this.set = promisify(client.set).bind(this.redisInstance);
+        this.get = promisify(this.redisInstance.get).bind(this.redisInstance);
+        this.set = promisify(this.redisInstance.set).bind(this.redisInstance);
     }
 }
 
